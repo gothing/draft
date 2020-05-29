b30644e88a243aa2c6f14588dcd8e9ea279f020b
Lebedev Konstantin <ibnrubaxa@gmail.com>
2020-05-29 15:14:52 +0300

    * upd

--- access.go
+++ access.go
@@ -7,15 +7,25 @@ type AccessType string
 var Access = struct {
 	All          AccessType
 	Auth         AccessType
+	Anonymous    AccessType
 	AuthOptional AccessType
 	Cookie       AccessType
 	AuthCookie   AccessType
 	Signature    AccessType
+	BasicAuth    AccessType
+	OAuth2       AccessType
+	Token        AccessType
+	Session      AccessType
 }{
 	"all",
 	"auth",
+	"anonymous",
 	"auth:optional",
 	"cookie",
 	"auth:cookie",
 	"signature",
+	"basic:auth",
+	"oauth2",
+	"token",
+	"session",
 }
