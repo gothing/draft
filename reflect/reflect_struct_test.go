5716323e3ade1884d428de888c3fb4d8b55a63ff
Denis Zinovyev <zinoviev.denis@yandex.ru>
2021-02-24 21:54:15 +0300

    required_group for parameters

--- reflect/reflect_struct_test.go
+++ reflect/reflect_struct_test.go
@@ -1,6 +1,7 @@
 package reflect_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/gothing/draft/reflect"
@@ -25,6 +26,67 @@ func TestStructWithJSONTag(t *testing.T) {
 	})
 
 	assert.Equal(t, []string{"access_token", "ClientID"}, v.Keys())
+}
+
+type StructWithRequiredTags struct {
+	RequiredField  int    `required:"true"`
+	RequiredField1 string `required:"true,grp"`
+	RequiredField2 uint64 `required:"true,grp"`
+	OptionalField  uint   `required:"false"`
+	ExtraField     bool
+}
+
+func TestStructWithRequiredTag(t *testing.T) {
+	v := reflect.Get(
+		StructWithRequiredTags{},
+		reflect.Options{
+			SnakeCase: true,
+		},
+	)
+
+	jsonObj, err := json.Marshal(v)
+	assert.NoError(t, err)
+
+	var m map[string]interface{}
+	assert.NoError(t, json.Unmarshal(jsonObj, &m))
+
+	fields := []struct {
+		Name          string
+		Required      bool
+		RequiredGroup string
+	}{
+		{
+			Name:     "required_field",
+			Required: true,
+		},
+		{
+			Name:          "required_field1",
+			Required:      true,
+			RequiredGroup: "grp",
+		},
+		{
+			Name:          "required_field2",
+			Required:      true,
+			RequiredGroup: "grp",
+		},
+		{
+			Name: "optional_field",
+		},
+		{
+			Name: "extra_field",
+		},
+	}
+
+	for _, field := range fields {
+		nested := m["nested"].([]interface{})
+		for _, f := range nested {
+			f := f.(map[string]interface{})
+			if f["name"] == field.Name {
+				assert.Equal(t, field.Required, f["required"])
+				assert.Equal(t, field.RequiredGroup, f["required_group"])
+			}
+		}
+	}
 }
 
 func xTestStructComposed(t *testing.T) {
